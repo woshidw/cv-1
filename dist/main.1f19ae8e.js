fd669ea8161d3db71c16c512087b125d032fe6d0
duwei <1069646435@qq.com>
2021-01-21 21:05:09 +0800

    bulid

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -180,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7556" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4073" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -357,4 +357,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
---
+++ src/README.md
@@ -0,0 +1,6 @@
+# 部署方式
+每次改完代码，必须运行这一行，才能正确请求JS和CSS。
+
+```
+parcel bulid src/index.html --public-url .
+```
